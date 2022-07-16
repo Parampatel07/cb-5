55e9d01149c3b94b9fd9b77f6b824da222231a31
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-07-16 16:45:27 +0530

    multiarray

--- multiarray.c
+++ multiarray.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 void main()
 {
-    int student[5][5], count = 0, std_count = 0;
-
+    int student[5][5], count = 0, std_count = 0, total[5];
     for (std_count = 0; std_count < 5; std_count++)
     {
         printf("Enter marks for student %d \n", std_count + 1);
@@ -14,12 +13,24 @@ void main()
             scanf("%d", &student[std_count][count]);
         }
     }
-    for(std_count=0;std_count<5;std_count++)
+    for (std_count = 0; std_count < 5; std_count++)
     {
-        printf("\nMarks for student %d are \n",std_count+1);
+        printf("\nMarks for student %d are \n", std_count + 1);
         for (count = 0; count < 5; count++)
         {
             printf("\nMakrs of subject %d are %d", count + 1, student[std_count][count]);
         }
+    }
+    for (std_count = 0; std_count < 5; std_count++)
+    {
+        total[std_count] = 0;
+        for (count = 0; count < 5; count++)
+        {
+            total[std_count] = total[std_count] + student[std_count][count];
+        }
+    }
+    for(count=0;count<5;count++)
+    {
+        printf("\n value of total for student %d is %d",count+1,total[count]);
     }
 }
